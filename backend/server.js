f2b564f0cf314fcbb4218d089265291451050391
Ibna Sina Sami <ibnasinasami@gmail.com>
2026-02-22 14:38:29 +0600

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://news-portal-livid.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
